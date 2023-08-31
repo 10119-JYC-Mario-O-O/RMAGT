9bea9b9897aa85b6a72ca1ba9f7a3bcbb779b817
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-09-01 08:55:46 +0900

    Test

--- Data/Code/index.js
+++ Data/Code/index.js
@@ -1,6 +1,6 @@
-document.write('<script src="Load/Value.js"></script>');
-document.write('<script src="Load/EventListener.js"></script>');
-document.write('<script src="Load/Image.js"></script>');
-document.write('<script src="Load/Audio.js"></script>');
-document.write('<script src="Run/runCode.js"></script>');
-document.write('<script src="Run/runCodeWithFPS.js"></script>');
+document.write('<script src="Data/Load/Value.js"></script>');
+document.write('<script src="Data/Load/EventListener.js"></script>');
+document.write('<script src="Data/Load/Image.js"></script>');
+document.write('<script src="Data/Load/Audio.js"></script>');
+document.write('<script src="Data/Run/runCode.js"></script>');
+document.write('<script src="Data/Run/runCodeWithFPS.js"></script>');
