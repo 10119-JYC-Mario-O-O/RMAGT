3c3f5a539dfb784619aaf736bb522a08f4790fa3
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-09-01 08:53:03 +0900

    Test

---
+++ Data/Code/Run/runCodeWithFPS.js
@@ -0,0 +1,14 @@
+function runCodeWithFPS () {  
+    requestAnimationFrame(runCodeWithFPS);
+    
+    now = Date.now();
+    delta = now - then;
+    
+    if (delta > interval) {
+        then  =  now  -  ( delta  %  interval ) ;
+
+        runCode();
+    }
+}
+
+runCodeWithFPS();
---
+++ Data/Code/index.js
@@ -0,0 +1,6 @@
+document.write('<script src="Load/Value.js"></script>');
+document.write('<script src="Load/EventListener.js"></script>');
+document.write('<script src="Load/Image.js"></script>');
+document.write('<script src="Load/Audio.js"></script>');
+document.write('<script src="Run/runCode.js"></script>');
+document.write('<script src="Run/runCodeWithFPS.js"></script>');
