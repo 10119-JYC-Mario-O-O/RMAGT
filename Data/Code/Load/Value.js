1a1143dc9dfd0942327c8783eb75537959cf06e6
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-09-01 08:40:58 +0900

    Test

--- Data/Code/Load/Value.js
+++ Data/Code/Load/Value.js
@@ -8,3 +8,7 @@ canvas.height = screen.availHeight;
 const ctx = canvas.getContext(`2d`);
 
 let w = false, a = false, s = false, d = false, j = false, k = false, l = false, enterKey = false, mouseClick = false;
+
+let fps = 60, interval = 1000 / fps;
+let delta, conter = 0;
+let now, then = Date.now(), savedTime = Date.now();
--- Data/Code/runCodeWithFPS.js
+++ Data/Code/runCodeWithFPS.js
@@ -2,18 +2,9 @@ document.write('<script src="Load/Value.js"></script>');
 document.write('<script src="Load/EventListener.js"></script>');
 document.write('<script src="Load/Image.js"></script>');
 document.write('<script src="Load/Audio.js"></script>');
-document.write('<script src="Load/runCode.js"></script>');
+document.write('<script src="runCode.js"></script>');
 
-function runCodeWithFPS () {
-    let fps = 60;
-    let now;
-    let then = Date.now();
-    let interval = 1000 / fps;
-    let delta;
-
-    let conter = 0;
-    let savedTime = Date.now();
-  
+function runCodeWithFPS () {  
     function setFPS() {
         requestAnimationFrame(setFPS);
      
