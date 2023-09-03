fef7619814e1b979ca834c15f33fe96465c4e7a3
10119-JYC-Mario-O-O <toadchen83@gmail.com>
2023-09-03 20:14:54 +0900

    Test

--- Data/Code/Run/runCode.js
+++ Data/Code/Run/runCode.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.write('<script src="Data/Code/Run/Game/Title_Screen.js"></script>');
 
 function runCode () {
@@ -13,16 +12,4 @@ function runCode () {
     // }
 
     Title_Screen();
-=======
-function runCode () {
-    conter++;
-            
-    if (conter >= 60) {
-        console.log(Date.now() - savedTime);
-
-        savedTime = Date.now();
-
-        conter = 0;
-    }
->>>>>>> 94529b0b4ca41d8dcde456b9f561826e91408deb
 }
--- Data/Code/index.js
+++ Data/Code/index.js
@@ -2,9 +2,5 @@ document.write('<script src="Data/Code/Load/Value.js"></script>');
 document.write('<script src="Data/Code/Load/EventListener.js"></script>');
 document.write('<script src="Data/Code/Load/Image.js"></script>');
 document.write('<script src="Data/Code/Load/Audio.js"></script>');
-<<<<<<< HEAD
 
-=======
-document.write('<script src="Data/Code/Run/runCode.js"></script>');
->>>>>>> 94529b0b4ca41d8dcde456b9f561826e91408deb
 document.write('<script src="Data/Code/Run/runCodeWithFPS.js"></script>');
