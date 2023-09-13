a05f5ecaa97463263b200d1df79968b61dee980e
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-09-13 00:01:31 +0000

    Test

--- Data/Code/Run/Game/Physics/Ground.js
+++ Data/Code/Run/Game/Physics/Ground.js
@@ -2,8 +2,8 @@ let AllBlockedGround = [0x01];
 
 //PBG = PlayerBackGround
 function GroundDetection () {                                      
-    let PBGX = PlayerRealX / 80, PBGY = PlayerRealY / 80,
-        PBGSideX = PBGX + PlayerWidth / 80, PBGSideY = PBGY + PlayerHeight / 80;
+    let PBGX = Math.floor(PlayerRealX / 80), PBGY = Math.floor(PlayerRealY / 80),
+        PBGSideX = Math.floor(PBGX + PlayerWidth / 80), PBGSideY = Math.floor(PBGY + PlayerHeight / 80);
 
         // drawImage(TestGround, 0, 0, 1440, 1440, Math.PBGX * 80, Math.PBGY * 80, 80, 80);
         // drawImage(TestGround, 0, 0, 1440, 1440, Math.PBGSideX * 80, Math.PBGSideY * 80, 80, 80);
