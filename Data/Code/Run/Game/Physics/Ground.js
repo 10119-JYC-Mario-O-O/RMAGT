46dc589281123132f2435976b1cf730fef2029d1
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-09-17 00:42:26 +0900

    Test

--- Data/Code/Run/Game/Physics/Ground.js
+++ Data/Code/Run/Game/Physics/Ground.js
@@ -1,15 +1,14 @@
 //PBG = PlayerBackGround
 function GroundDetection () {
-    let hasTouchedWall = false;
     let PBGX = Math.floor(PlayerRealX / 80), PBGY = Math.floor(PlayerRealY / 80),
-        PBGSideX = Math.floor((PlayerRealX + PlayerWidth) / 80), PBGSideY = Math.floor((PlayerRealY + PlayerHeight) / 80);
+        PBGSideX = Math.floor((PlayerRealX + PlayerWidth - 1) / 80), PBGSideY = Math.floor((PlayerRealY + PlayerHeight) / 80);
 
     if (DebugMod) {
         drawImage(TestSpr, 0, 0, 1440, 1440, PBGX * 80, PBGY * 80, 80, 80);
         drawImage(TestSpr, 0, 0, 1440, 1440, PBGSideX * 80, PBGY * 80, 80, 80);
     }
 
-    // Bonkable_Ceiling : Standable Ground
+    // set isBonked
     if ((Bonkable_Ceiling.includes(loadedLevel[PBGX + PBGY * loadedLevel_Width]) || 
         Bonkable_Ceiling.includes(loadedLevel[PBGSideX + PBGY * loadedLevel_Width])) && 
         PlayerMoveY <= 0) {
@@ -30,26 +29,24 @@ function GroundDetection () {
         }
     }
 
-    if (!hasTouchedWall) {
-        // Bonkable_Ceiling : Standable Ground
-        if ((Bonkable_Ceiling.includes(loadedLevel[PBGX + PBGY * loadedLevel_Width]) || 
-            Bonkable_Ceiling.includes(loadedLevel[PBGSideX + PBGY * loadedLevel_Width])) && 
-            PlayerMoveY <= 0) {
-            
-            PlayerRealY = PBGSideY * 80;
-            PlayerMoveY = 0;
-            
-            isBonked = true;
-        } else if ((Standable_Ground.includes(loadedLevel[PBGX + PBGSideY * loadedLevel_Width]) || 
-            Standable_Ground.includes(loadedLevel[PBGSideX + PBGSideY * loadedLevel_Width])) && 
-            PlayerMoveY >= 0) {
-            
-            isOnGround = true;
-            PlayerMoveY = 0;
-            
-            isBonked = false;
+    // Bonkable_Ceiling : Standable Ground
+    if ((Bonkable_Ceiling.includes(loadedLevel[PBGX + PBGY * loadedLevel_Width]) || 
+        Bonkable_Ceiling.includes(loadedLevel[PBGSideX + PBGY * loadedLevel_Width])) && 
+        PlayerMoveY <= 0) {
+        
+        PlayerRealY = PBGSideY * 80;
+        PlayerMoveY = 0;
+        
+        isBonked = true;
+    } else if ((Standable_Ground.includes(loadedLevel[PBGX + PBGSideY * loadedLevel_Width]) || 
+        Standable_Ground.includes(loadedLevel[PBGSideX + PBGSideY * loadedLevel_Width])) && 
+        PlayerMoveY >= 0) {
+        
+        isOnGround = true;
+        PlayerMoveY = 0;
+        
+        isBonked = false;
 
-            PlayerRealY = (PBGSideY * 80 - 80) + (80 - PlayerHeight);
-        }
+        PlayerRealY = (PBGSideY * 80 - 80) + (80 - PlayerHeight);
     }
 }
