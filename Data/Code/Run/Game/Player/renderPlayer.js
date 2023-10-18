1db28aaa98b759317c6634dd3ab95aab1352395a
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-10-18 09:49:26 +0900

    Test

--- Data/Code/Run/Game/Player/renderPlayer.js
+++ Data/Code/Run/Game/Player/renderPlayer.js
@@ -1,3 +1,13 @@
 function renderPlayer() {
     PlayerAnimationFrame += Math.abs(PlayerMoveX) * 8 / 15 - 1;
+    
+    if (PlayerAnimationFrame <= 0) {
+        PlayerAnimationFrame = 0;
+    }
+    
+    loadedText[33] = Math.floor(PlayerAnimationFrame / 100);
+    loadedText[34] = Math.floor(PlayerAnimationFrame / 10) - A * 10;
+    loadedText[35] = PlayerAnimationFrame - A * 100 - B * 10;
+
+    drawImage(R, 0, 0, 256, 256, PlayerDrawX, PlayerDrawY, PlayerWidth, PlayerHeight);
 }
