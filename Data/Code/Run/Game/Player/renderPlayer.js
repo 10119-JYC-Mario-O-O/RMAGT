8516df41d32e183591c311806389af31478166de
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-11-10 15:22:03 +0900

    Test

--- Data/Code/Run/Game/Player/renderPlayer.js
+++ Data/Code/Run/Game/Player/renderPlayer.js
@@ -1,5 +1,5 @@
 function renderPlayer() {
-    PlayerAnimationFrame += Math.abs(PlayerMoveX) * 8 / 15 - 1;
+    PlayerAnimationFrame += Math.abs(PlayerMoveX) * 8 - 10;
     
     if (PlayerAnimationFrame <= 0) {
         PlayerAnimationFrame = 0;
@@ -8,11 +8,18 @@ function renderPlayer() {
     let A = Math.floor(PlayerAnimationFrame / 100);
     loadedText[33] = A;
     
-    let B = Math.floor(PlayerAnimationFrame / 10) - A * 10;
-    loadedText[34] = B
+    let B = Math.floor(PlayerAnimationFrame / 10) - A * 10;3
+    loadedText[34] = B;
     
     let C = Math.floor(PlayerAnimationFrame) - A * 100 - B * 10;
     loadedText[35] = C;
 
-    drawImage(PlayerImage, PlayerAnimationState * 80, PowerUpState * 80 * 3 + 80 - 80 * (s && !w) + 80 * (w), PlayerWidth, PlayerHeight, PlayerDrawX, PlayerDrawY, PlayerWidth, PlayerHeight);
+
+
+    drawImage(PlayerImage, 
+              PlayerAnimationState * 80 + PlayerAnimationState * 8, PowerUpState * 88 * 3 + 88 - 88 * (s && !w) + 88* (w), 
+              PlayerWidth * (Math.floor(PlayerWidth / 16) == PlayerWidth / 16) + 80 * (Math.floor(PlayerWidth / 16) != PlayerWidth / 16), 
+              PlayerHeight * (Math.floor(PlayerHeight / 16) == PlayerHeight / 16) + 80 * (Math.floor(PlayerHeight / 16) != PlayerHeight / 16), 
+              PlayerDrawX,PlayerDrawY, 
+              PlayerWidth, PlayerHeight);
 }
