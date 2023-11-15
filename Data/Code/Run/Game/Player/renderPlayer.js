80fa2cba4baaa07f8ce168bed3efa8f4c8d166fa
Aha <118600849+10119-JYC-Mario-O-O@users.noreply.github.com>
2023-11-15 09:58:12 +0900

    Test

--- Data/Code/Run/Game/Player/renderPlayer.js
+++ Data/Code/Run/Game/Player/renderPlayer.js
@@ -1,8 +1,8 @@
 function renderPlayer() {
     PlayerAnimationFrame += Math.abs(PlayerMoveX) * 2 - 1;
     
-    if (PlayerAnimationFrame <= 0) {
-        PlayerAnimationFrame = 0;
+    if (Math.abs(PlayerMoveX) * 2 - 1 < 0) {
+        PlayerAnimationState = 0;
     }
 
     let A = Math.floor(PlayerAnimationFrame / 100);
@@ -14,7 +14,15 @@ function renderPlayer() {
     let C = Math.floor(PlayerAnimationFrame) - A * 100 - B * 10;
     loadedText[35] = C;
 
+    if (PlayerAnimationFrame >= 60) {
+        PlayerAnimationState++;
 
+        PlayerAnimationFrame = 0;
+
+        if (PlayerAnimationState < 2 || PlayerAnimationState >= PlayerSpriteSheetLeftX) {
+            PlayerAnimationState = 2;
+        }
+    }
 
     drawImage(PlayerImage, 
               PlayerAnimationState * 80 + PlayerAnimationState * 8 + 88 * PlayerSpriteSheetLeftX * (!PlayerFacingRight), PowerUpState * 88 * 3 + 88 - 88 * (s && !w) + 88* (w), 
